e95e9a1d5ededf94d26500c29c46fdfd9ab49039
KenjxJJ <joelkiggwe94@gmail.com>
2022-10-10 01:12:39 +0300

    Modify the UI of the applicants(candidates)

--- pages/applicants.tsx
+++ pages/applicants.tsx
@@ -26,6 +26,7 @@ const Applicants: NextPage = () => {
                         <DropDown imageUrl={ProfileImage} classes={styles.dropDown} text={"Ready to Interview"} />
                     </div>
                 </section>
+                
                 <section className="grid grid-flow-row grid-cols-7 gap-6 mb-[50px]">
                     <DropDown classes={styles.dropDown} text={"Location"} />
                     <DropDown classes={styles.dropDown} text={"Salary"} />
@@ -33,16 +34,89 @@ const Applicants: NextPage = () => {
                     <DropDown classes={styles.dropDown} text={"Job type"} />
                     <DropDown classes={styles.dropDown} text={"Job category"} />
                 </section>
-                <section className="grid grid-flow-row grid-cols-3">
-                    <header className='grid grid-flow-col grid-cols-1 col-start-3 grid-rows-auto mb-10 pr-5 place-items-center'>
-                        <span className="underline col-span-1  text-primary_green hover:cursor-pointer">
+
+                <section className="grid grid-flow-row grid-cols-8">
+                    <header className='grid grid-flow-col col-start-6 col-span-3 grid-rows-auto justify-self-end mb-10  place-items-center'>
+                        <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer text-center">
                             <CheckBox classes={styles.checkbox} />
-                            Select All
-                        </span>
-                        <span className="col-span-1  text-primary_green hover:cursor-pointer">Accept</span>
-                        <span className="col-span-1 text-primary_green hover:cursor-pointer">Reject</span>
+                            Select all
+                        </p>
+                        <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer">Accept</p>
+                        <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer">Reject</p>
                     </header>
                     {/* Wrapper for each item */}
+                    <div className='grid grid-flow-col grid-cols-7 col-span-full gap-2 grid-rows-auto justify-items-end mb-[52px]'>
+
+                        <CheckBox classes={styles.checkbox} />
+                        <div className={styles.applicantWrapper}>
+                            <span className={styles.applicantDetails}>
+                                <span className="w-16 h-16 col-span-1">
+                                    <Image src={FrontDevLady} />
+                                </span>
+                                <section className='col-span-3'>
+                                    <h3 className='text-base font-semibold mb-[16px] '>JMary Dekoo</h3>
+                                    <p className="text-base mb-[22px] font-normal">Front end Developer</p>
+                                    <a href="/" className='text-sm underline font-semibold text-primary_green'>View Profile</a>
+                                </section>
+                            </span>
+
+                            <span className={styles.actionButtons}>
+                                <Button
+                                    type="filled"
+                                    bgColor="primary_green"
+                                    color="white"
+                                    text="Accept"
+                                    classes="w-max px-3 h-10 md:px-6 xl:px-12 rounded-md text-sm 
+                                capitalize text-white bg-primary_green hover:opacity-80 leading-[22px]"
+                                />
+
+                                <Button
+                                    type="bordered"
+                                    bgColor="white-2"
+                                    color="green"
+                                    text="Reject"
+                                    classes="w-max px-3  h-10 md:px-6 xl:px-12 rounded-md border-2 border-green-900
+                                 text-sm capitalize text-primary_green bg-white hover:opacity-80 leading-[22px]"
+                                />
+                            </span>
+                        </div>
+                    </div>
+                    <div className='grid grid-flow-col grid-cols-7 col-span-full gap-2 grid-rows-auto justify-items-end mb-[52px]'>
+
+                        <CheckBox classes={styles.checkbox} />
+                        <div className={styles.applicantWrapper}>
+                            <span className={styles.applicantDetails}>
+                                <span className="w-16 h-16 col-span-1">
+                                    <Image src={FrontDevLady} />
+                                </span>
+                                <section className='col-span-3'>
+                                    <h3 className='text-base font-semibold mb-[16px] '>JMary Dekoo</h3>
+                                    <p className="text-base mb-[22px] font-normal">Front end Developer</p>
+                                    <a href="/" className='text-sm underline font-semibold text-primary_green'>View Profile</a>
+                                </section>
+                            </span>
+
+                            <span className={styles.actionButtons}>
+                                <Button
+                                    type="filled"
+                                    bgColor="primary_green"
+                                    color="white"
+                                    text="Accept"
+                                    classes="w-max px-3 h-10 md:px-6 xl:px-12 rounded-md text-sm 
+                                capitalize text-white bg-primary_green hover:opacity-80 leading-[22px]"
+                                />
+
+                                <Button
+                                    type="bordered"
+                                    bgColor="white-2"
+                                    color="green"
+                                    text="Reject"
+                                    classes="w-max px-3  h-10 md:px-6 xl:px-12 rounded-md border-2 border-green-900
+                                 text-sm capitalize text-primary_green bg-white hover:opacity-80 leading-[22px]"
+                                />
+                            </span>
+                        </div>
+                    </div>
                     <div className='grid grid-flow-col grid-cols-7 col-span-full gap-2 grid-rows-auto justify-items-end mb-[52px]'>
 
                         <CheckBox classes={styles.checkbox} />
