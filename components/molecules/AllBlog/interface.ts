08469777970a7437edf699f892df595669844a8d
Ikoyo Daniel Omovigho <dannirolands@gmail.com>
2022-10-11 23:29:34 +0100

    feat: implemented blog screen

---
+++ components/molecules/AllBlog/interface.ts
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image";
+
+export interface ArticleProp  {
+    featuredImage: string | StaticImageData;
+    title:string ;
+    excerpt: string;
+    category: string;
+    author: {
+      image: string;
+      fullname: string;
+    },
+    meta: {
+      publishedDate:string;
+      readTime: string;
+    },
+}
