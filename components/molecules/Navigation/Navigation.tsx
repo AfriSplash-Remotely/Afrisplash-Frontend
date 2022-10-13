943a56b391575d9b35f37b2230453e922c1f096d
Mathilda Imadojiemu <mathilda.imadojiemu@mymdoc.com>
2022-10-13 07:10:33 +0100

    resolved blog conflicts

--- components/molecules/Navigation/Navigation.tsx
+++ components/molecules/Navigation/Navigation.tsx
@@ -73,7 +73,6 @@ const Navigation = ():JSX.Element => {
     }
   };
   return (
-<<<<<<< HEAD
     <div className={`w-full h-20 sticky top-0 z-50 bg-white`}>
       <div className={`relative h-20 ${styles.wrapper}`}>
         <header className="w-11/12 relative z-50 md:w-11/12 xl:w-10/12 mx-auto h-full flex justify-between  items-center">
@@ -121,51 +120,6 @@ const Navigation = ():JSX.Element => {
             <div className="flex lg:hidden h-full items-center">
               <Bars3Icon className="w-8 h-6" />
             </div>
-=======
-    <div className="h-20 ">
-      <header className="mx-auto flex h-full w-11/12 items-center justify-between md:w-11/12  xl:w-10/12">
-        <div className="flex h-full w-1/12 items-center ">
-          <Image src="/main_logo.svg" height="70%" width="70%" />
-        </div>
-        <nav className="hidden h-full items-center lg:flex ">
-          <ul className="flex lg:space-x-3 xl:space-x-5">
-            {header.map((item: any, index: number) => (
-              <Link href={item.link} key={index}>
-                <li
-                  onClick={() => handleMenuClick(item, index)}
-                  className={`cursor-pointer text-sm capitalize hover:text-primary_green hover:underline hover:underline-offset-4 ${
-                    item.active &&
-                    "text-primary_green underline underline-offset-4"
-                  }`}
-                >
-                  {item.title}
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </nav>
-        <div className="flex items-center space-x-4">
-          <div className="hidden space-x-4 md:flex">
-            <Button
-              type="filled"
-              bgColor="dark_blue"
-              color="white"
-              classes="w-36 h-10  md:w-28 xl:w-36 rounded-md text-sm capitalize text-white bg-dark_blue hover:opacity-80"
-            >
-              sign in
-            </Button>
-            <Button
-              type="bordered"
-              color="dark_blue"
-              borderColor="dark_blue"
-              classes="w-36 md:w-28 xl:w-36 rounded-md capitalize h-10 text-sm text-dark_blue border border-dark_blue hover:bg-dark_blue hover:text-white  hover:opacity-80"
-            >
-              Post Jobs
-            </Button>
-          </div>
-          <div className="flex h-full items-center lg:hidden">
-            <Bars3Icon className="h-6 w-8" />
->>>>>>> 06ce0d1117f9bb3e2c36fc3f9210a4d37508ef4c
           </div>
         </header>
       </div>
