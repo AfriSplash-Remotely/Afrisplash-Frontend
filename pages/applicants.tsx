26de63664346b8c4be58d83c49b2347efef3db2d
KenjxJJ <joelkiggwe94@gmail.com>
2022-10-19 01:26:26 +0300

    Add search functionality

--- pages/applicants.tsx
+++ pages/applicants.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import styles from '../styles/Applicants.module.scss';
 import Image, { StaticImageData } from 'next/image';
 import { BellIcon, GiftIcon } from '@heroicons/react/24/outline';
+import AdminNavigation from 'components/molecules/Navigation/AdminNavigation';
+
 
 import Button from 'components/atoms/Button/Button';
 import CheckBox from 'components/atoms/CheckBox/CheckBox';
@@ -36,7 +38,7 @@ const Applicants: NextPage = () => {
         },
         {
             id: 2,
-            name: "JMary Dekkoo",
+            name: "Jane Miysey",
             imageUrl: FrontDevLady,
             role: "Front End Developer",
             isSelected: false
@@ -45,18 +47,24 @@ const Applicants: NextPage = () => {
             id: 3,
             name: "JMary Dekkoo",
             imageUrl: FrontDevLady,
-            role: "Front End Developer",
+            role: "DevOps Engineer",
             isSelected: false
         },
         {
             id: 4,
-            name: "JMary Dekkoo",
+            name: "Jane Heuwolo",
+            imageUrl: FrontDevLady,
+            role: "Backend Developer",
+            isSelected: false
+        },
+        {
+            id: 5,
+            name: "Hellen Miysey",
             imageUrl: FrontDevLady,
             role: "Front End Developer",
             isSelected: false
         }
     ])
-
     const [applicantLabels, setApplicantLabels] = useState([
         {
             label: 'Location',
@@ -76,14 +84,21 @@ const Applicants: NextPage = () => {
         }
     ])
 
+    const [queriedApplicants, setQueriedApplicants] = useState([...applicants])
 
     // QuerySearch on Applicant list
     const handleQueryApplicantsInList = (e: any) => {
         // Obtain the values from the search bar
         let queryValue = e.target.value;
-        // Obtain applicants based on  the query
-        // Update list
-        // Hence displaying query results
+        let queryApplicants;
+
+        if (queryValue !== null) {
+            // Obtain applicants based on  the query
+            queryApplicants = applicants.filter((applicant) => (applicant.name.includes(queryValue) || applicant.role.includes(queryValue)));
+            // Update list 
+            // Hence displaying query results
+            setQueriedApplicants([...queryApplicants]);
+        }
     }
 
     // Obtain store of Applicants from store
@@ -153,94 +168,108 @@ const Applicants: NextPage = () => {
 
     return (
         <>
-            <main className='grid grid-flow-row grid-cols-1 grid-rows-1 gap-2 pt-8 px-16 '>
-                <section className='grid  grid-flow-row grid-cols-6 gap-3 grid-rows-1 mb-[60px]'>
-                    <SearchBar classes={styles.searchBar}
-                        onChange={(e): any => handleQueryApplicantsInList(e)} />
-                    <div className="grid grid-flow-col col-start-5 col-end-7 place-items-center">
-                        <GiftIcon className="h-6 w-6 hover:cursor-pointer" />
-                        <BellIcon className="h-6 w-6 hover:cursor-pointer" />
-                        <DropDown imageUrl={ProfileImage} classes={styles.dropDown} text={"Ready to Interview"}
-                            onClick={() => { setIsOpen(!isOpen); }} isOpen={isOpen} />
-                    </div>
-                </section>
+            {/* AdminNavigation */}
+            <div className='grid grid-flow-col'>
+                <AdminNavigation />
 
-                <section className="grid grid-flow-row grid-cols-7 gap-6 mb-[50px]">
-                    {applicantLabels.map(({ label, isOpen }) => (
-                        <DropDown key={label} classes={styles.dropDown} text={label} onClick={() => {
-                            let newApplicantLabels: any[] = applicantLabels.map((applicant: { label: String, isOpen: Boolean }) => {
-                                if (applicant.label === label) applicant.isOpen = !isOpen;
-                                return applicant;
-                            });
-                            setApplicantLabels([...newApplicantLabels]);
-                        }} isOpen={isOpen} />
-                    ))}
-                </section>
-
-                <section className="grid grid-flow-row grid-cols-8">
-                    <header className='grid grid-flow-col col-start-6 col-span-3 grid-rows-auto justify-self-end mb-10 place-items-center'>
-                        {!makeSelectOptionsVisible && <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer text-center"
-                            onClick={() => setMakeSelectOptionsVisible(!makeSelectOptionsVisible)}>
-                            Select
-                        </p>}
-                        {makeSelectOptionsVisible && (<>
-                            <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer text-center">
-                                <CheckBox classes={styles.checkbox} onChange={(e) => { handleApplicantSelectionById({ e, _id: -1 }) }} />
-                                Select all
-                            </p>
-                            <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer" onClick={() => handleAcceptApplicants(-1)}>Accept</p>
-                            <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer" onClick={() => handleRejectApplicants(-1)}>Reject</p>
-                        </>
-                        )}
-                    </header>
-
-                    {applicants.length > 0 && applicants.map(({ id, name, role, imageUrl, isSelected }) => (
-                        <div key={id} className='grid grid-flow-col grid-cols-7 col-span-full gap-2 grid-rows-auto justify-items-end mb-[52px]'>
-
-                            {makeSelectOptionsVisible &&
-                                <CheckBox classes={styles.checkbox} onChange={(e) => { handleApplicantSelectionById({ e, _id: id }) }} />
-                            }
-                            <div className={styles.applicantWrapper} style={{ background: isSelected ? ' rgba(217, 222, 220, 1)' : 'none' }}>
-                                <span className={styles.applicantDetails}>
-                                    <span className="w-16 h-16 col-span-1">
-                                        <Image src={imageUrl} />
-                                    </span>
-                                    <section className='col-span-3'>
-                                        <h3 className='text-base font-semibold mb-[16px] '>{name}</h3>
-                                        <p className="text-base mb-[22px] font-normal capitalize">{role}</p>
-                                        <a href={`applicants/${id.toString()}`} className='text-sm underline font-semibold text-primary_green'>View Resume</a>
-                                    </section>
-                                </span>
-
-                                <span className={styles.actionButtons}>
-                                    <Button
-                                        onClick={() => handleApplicantByIdWithAppropriateAction("Accept", id)}
-                                        type="filled"
-                                        bgColor="primary_green"
-                                        color="white"
-                                        text="Accept"
-                                        classes="w-max px-3 h-10 md:px-6 xl:px-12 rounded-md text-sm 
-                                    capitalize text-white bg-primary_green hover:opacity-80 leading-[22px]"
-                                    />
-
-                                    <Button
-                                        onClick={() => handleApplicantByIdWithAppropriateAction("Reject", id)}
-                                        type="bordered"
-                                        bgColor="white-2"
-                                        color="green"
-                                        text="Reject"
-                                        classes="w-max px-3  h-10 md:px-6 xl:px-12 rounded-md border-2 border-green-900
-                                 text-sm capitalize text-primary_green bg-white hover:opacity-80 leading-[22px]"
-                                    />
-                                </span>
-
-                            </div>
+                <main className='grid grid-cols-1 gap-2 pt-8 px-16'>
+                    <section className='grid  grid-flow-row grid-cols-6 gap-3 mb-[60px] max-h-5'>
+                        <SearchBar classes={styles.searchBar}
+                            onChange={(e): any => handleQueryApplicantsInList(e)} />
+                        <div className="grid grid-flow-col col-start-5 col-end-7 place-items-center">
+                            <GiftIcon className="h-6 w-6 hover:cursor-pointer" />
+                            <BellIcon className="h-6 w-6 hover:cursor-pointer" />
+                            <DropDown imageUrl={ProfileImage} classes={styles.dropDown} text={"Ready to Interview"}
+                                onClick={() => { setIsOpen(!isOpen); }} isOpen={isOpen} />
                         </div>
-                    ))
-                    }
+                    </section>
 
-                </section>
-            </main>
+                    <section className="grid grid-flow-row grid-cols-6 gap-4 mb-[50px] max-h-5">
+                        {applicantLabels.map(({ label, isOpen }) => (
+                            <DropDown key={label} classes={styles.dropDown} text={label} onClick={() => {
+                                let newApplicantLabels: any[] = applicantLabels.map((applicant: { label: String, isOpen: Boolean }) => {
+                                    if (applicant.label === label) applicant.isOpen = !isOpen;
+                                    return applicant;
+                                });
+                                setApplicantLabels([...newApplicantLabels]);
+                            }} isOpen={isOpen} />
+                        ))}
+                    </section>
+
+                    {queriedApplicants.length > 0 && (
+                        <section className="grid grid-flow-row grid-cols-8 h-screen">
+                            <header className='grid grid-flow-col col-start-6 col-span-3 grid-rows-auto justify-self-end mb-10 place-items-center'>
+                                {!makeSelectOptionsVisible && <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer text-center"
+                                    onClick={() => setMakeSelectOptionsVisible(!makeSelectOptionsVisible)}>
+                                    Select
+                                </p>}
+                                {makeSelectOptionsVisible && (<>
+                                    <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer text-center">
+                                        <CheckBox classes={styles.checkbox} onChange={(e) => { handleApplicantSelectionById({ e, _id: -1 }) }} />
+                                        Select all
+                                    </p>
+                                    <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer" onClick={() => handleAcceptApplicants(-1)}>Accept</p>
+                                    <p className="m-4 col-span-1 text-primary_green hover:cursor-pointer" onClick={() => handleRejectApplicants(-1)}>Reject</p>
+                                </>
+                                )}
+                            </header>
+
+                            {queriedApplicants.map(({ id, name, role, imageUrl, isSelected }) => (
+                                <div key={id} className='grid grid-flow-col grid-cols-7 col-span-full gap-2 grid-rows-auto justify-items-end mb-[52px]'>
+
+                                    {makeSelectOptionsVisible &&
+                                        <CheckBox classes={styles.checkbox} onChange={(e) => { handleApplicantSelectionById({ e, _id: id }) }} />
+                                    }
+                                    <div className={styles.applicantWrapper} style={{ background: isSelected ? ' rgba(217, 222, 220, 1)' : 'none' }}>
+                                        <span className={styles.applicantDetails}>
+                                            <span className="w-16 h-16 col-span-1">
+                                                <Image src={imageUrl} />
+                                            </span>
+                                            <section className='col-span-3'>
+                                                <h3 className='text-base font-semibold mb-[16px] '>{name}</h3>
+                                                <p className="text-base mb-[22px] font-normal capitalize">{role}</p>
+                                                <a href={`applicants/${id.toString()}`} className='text-sm underline font-semibold text-primary_green'>View Resume</a>
+                                            </section>
+                                        </span>
+
+                                        <span className={styles.actionButtons}>
+                                            <Button
+                                                onClick={() => handleApplicantByIdWithAppropriateAction("Accept", id)}
+                                                type="filled"
+                                                bgColor="primary_green"
+                                                color="white"
+                                                text="Accept"
+                                                classes="w-max px-3 h-10 md:px-6 xl:px-12 rounded-md text-sm 
+                                    capitalize text-white bg-primary_green hover:opacity-80 leading-[22px]"
+                                            />
+
+                                            <Button
+                                                onClick={() => handleApplicantByIdWithAppropriateAction("Reject", id)}
+                                                type="bordered"
+                                                bgColor="white-2"
+                                                color="green"
+                                                text="Reject"
+                                                classes="w-max px-3  h-10 md:px-6 xl:px-12 rounded-md border-2 border-green-900
+                                 text-sm capitalize text-primary_green bg-white hover:opacity-80 leading-[22px]"
+                                            />
+                                        </span>
+
+                                    </div>
+                                </div>
+                            ))
+                            }
+
+                        </section>
+                    )}
+                    {queriedApplicants.length === 0 && (
+                        <section className="grid grid-flow-row grid-cols-8 h-screen">
+                            <header className='grid grid-flow-col col-start-1 col-span-5 justify-self-start py-10'>
+                                <p className="text-xl opacity-75">Sorry, no match found!</p>
+                            </header>
+                        </section>)}
+                </main>
+
+            </div>
         </>
     )
 }
