e4094eff237a75104324eb2863ce22e1af5f78b6
copy-ninja1 <emmajnr1000@gmail.com>
2022-10-24 10:57:25 +0100

    fix: recruiter layout

---
+++ pages/recruiter/jobs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import AdminLayout from "layouts/adminLayout";
+
+const Jobs: NextPage = () => {
+  return <AdminLayout>Jobs page</AdminLayout>;
+};
+
+export default Jobs;
