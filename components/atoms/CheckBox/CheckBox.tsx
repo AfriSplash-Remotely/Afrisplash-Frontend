3006fa7676311d036b6c6f337ed492885c4b88a4
KenjxJJ <joelkiggwe94@gmail.com>
2022-10-31 16:44:26 +0300

    First edit after review

--- components/atoms/CheckBox/CheckBox.tsx
+++ components/atoms/CheckBox/CheckBox.tsx
@@ -1,15 +1,18 @@
-interface CheckBoxProps {
-    classes?: string;
-    onChange?: (e:any) => void | any;
+import PropTypes, { InferProps } from "prop-types";
+
+const CheckBoxProps = {
+    classes: PropTypes.string,
+    onChange: PropTypes.func
 }
 
 
-const CheckBox = ({ classes, onChange }: CheckBoxProps) => {
+const CheckBox = ({ classes, onChange }: InferProps<typeof CheckBoxProps>) => {
     return (
         <>
-            <input type="checkbox" className={`${classes}`} onChange={onChange}  />
+            <input type="checkbox" className={`${classes}`} onChange={onChange} />
         </>
     )
 }
+
 
 export default CheckBox;
