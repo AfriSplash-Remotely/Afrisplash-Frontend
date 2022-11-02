643e3333e9710b447b5684a3fa21a7140f452745
copy-ninja1 <emmajnr1000@gmail.com>
2022-11-02 11:41:59 +0100

    fix: format code

--- components/atoms/CheckBox/CheckBox.tsx
+++ components/atoms/CheckBox/CheckBox.tsx
@@ -1,18 +1,16 @@
 import PropTypes, { InferProps } from "prop-types";
 
 const CheckBoxProps = {
-    classes: PropTypes.string,
-    onChange: PropTypes.func
-}
-
+  classes: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 const CheckBox = ({ classes, onChange }: InferProps<typeof CheckBoxProps>) => {
-    return (
-        <>
-            <input type="checkbox" className={`${classes}`} onChange={onChange} />
-        </>
-    )
-}
-
+  return (
+    <>
+      <input type="checkbox" className={`${classes}`} onChange={onChange} />
+    </>
+  );
+};
 
 export default CheckBox;
