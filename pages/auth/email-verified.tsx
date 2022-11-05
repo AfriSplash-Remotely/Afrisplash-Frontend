9392bf6af025c8f77a98bfdad0627313dc818dd8
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-11-05 09:38:00 +0100

    changed to Tailwind CSS & updated to Hooks

--- pages/auth/email-verified.tsx
+++ pages/auth/email-verified.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import EmailVerified from "components/EmailVerified/EmailVerified";
 import Logo from "components/Logo/Logo";
-import VectorBox from "components/VectorBox/VectorBox";
-import styles from "styles/Verify-email.module.scss";
+import VectorBox2 from "components/VectorBox/VectorBox2";
 
 function App() {
   return (
@@ -13,15 +12,15 @@ function App() {
         <title>Afrisplash</title>{" "}
         <meta name="description" content="Email verified page" />{" "}
       </Head>{" "}
-      <body className={styles.body}>
+      <body className="max-w-[1440px] h-[1027px] bg-[#FFF] mr-auto ml-auto">
         {" "}
-        <div className={styles.logoHeader}>
+        <div className="w-full flex flex-row flex-wrap mr-auto ml-auto">
           {" "}
           <Logo />{" "}
         </div>{" "}
-        <div className={styles.borderBox}>
+        <div className="max-w-[660px] h-[739px] l-[320px] bg-[#fff] shadow-[4px_4px_32px_rgba(0,0,0,0.1)] backdrop-opacity-20 rounded-[24px] w-full mt-12 flex box-border mr-auto ml-auto">
           {" "}
-          <VectorBox /> <EmailVerified />{" "}
+          <VectorBox2 /> <EmailVerified />{" "}
         </div>{" "}
       </body>{" "}
     </>
