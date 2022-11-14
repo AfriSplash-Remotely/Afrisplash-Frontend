0822ae4e79505be3b935fece5228c6d8da5b3204
sammy-code98 <sammysmart98@yahoo.com>
2022-11-14 21:39:57 +0100

    feat: mentor listing screen implementation

--- pages/dashboard/forum.tsx
+++ pages/dashboard/forum.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { NextPage } from "next";
+import Tabs from "components/Forum/TabComponent/Tabs"
 import AdminLayout from "layouts/adminLayout";
 
+
 const Forum: NextPage = () => {
-  return <AdminLayout>Forum page</AdminLayout>;
+  return <AdminLayout>
+    <div className="mt-4">
+      <Tabs />
+    </div>
+
+
+  </AdminLayout>;
 };
 
 export default Forum;
