043126602bfe1bac61954900d245d16616bcfed7
sammy-code98 <sammysmart98@yahoo.com>
2022-11-18 17:20:46 +0100

    fix: move svgs to files and import them as components

--- components/Forum/MentorCardComponent/MentorCard.tsx
+++ components/Forum/MentorCardComponent/MentorCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import PropTypes, { InferProps } from "prop-types";
+import bagIcon from "assets/forum/bagIcon.svg"
+import chatIcon from "assets/forum/chatIcon.svg"
+import heartIcon from "assets/forum/heartIcon.svg"
+import supportIcon from "assets/forum/supportIcon.svg"
 
 const MentorCardProps = {
     name: PropTypes.string,
@@ -17,15 +21,15 @@ export default function MentorCard({ name, role, position, flag, src }: InferPro
             style={{ backgroundImage: `url(${src})` }}
         >
             <div className='flex justify-between'>
-                <div className='p-2  rounded-full mentorCardBtn cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-7 h-7">
-                        <path fillRule="evenodd" d="M12 2.25c-2.429 0-4.817.178-7.152.521C2.87 3.061 1.5 4.795 1.5 6.741v6.018c0 1.946 1.37 3.68 3.348 3.97.877.129 1.761.234 2.652.316V21a.75.75 0 001.28.53l4.184-4.183a.39.39 0 01.266-.112c2.006-.05 3.982-.22 5.922-.506 1.978-.29 3.348-2.023 3.348-3.97V6.741c0-1.947-1.37-3.68-3.348-3.97A49.145 49.145 0 0012 2.25zM8.25 8.625a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zm2.625 1.125a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
-                    </svg>
+                <div className='p-4  rounded-full mentorCardBtn cursor-pointer'>
+                    <div className='relative h-6 w-6'>
+                        <Image src={chatIcon} alt='chatIcon' layout='fill' />
+                    </div>
                 </div>
-                <div className='p-2  rounded-full mentorCardBtn cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-7 h-7">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                    </svg>
+                <div className='p-4 rounded-full mentorCardBtn cursor-pointer'>
+                    <div className='relative h-6 w-6'>
+                        <Image src={heartIcon} alt='heartIcon' />
+                    </div>
                 </div>
             </div>
             <div className='py-4 mt-3'>
@@ -43,19 +47,17 @@ export default function MentorCard({ name, role, position, flag, src }: InferPro
                 </div>
                 <div className='mt-2 flex items-center'>
                     <div className='p-2  rounded-md mentorCardBtn'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="w-5 h-5">
-                            <path fillRule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clipRule="evenodd" />
-                            <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
-                        </svg>
+                        <div className='relative h-5'>
+                            <Image src={bagIcon} alt='bagIcon' />
+                        </div>
                     </div>
                     <h5 className='pl-2 text-lg text-white_2 font-medium'>{position}</h5>
                 </div>
                 <div className='mt-2 flex items-center'>
                     <div className='p-2  rounded-md mentorCardBtn'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" className="w-5 h-5">
-                            <path fillRule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clipRule="evenodd" />
-                            <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
-                        </svg>
+                        <div className='relative h-5'>
+                            <Image src={supportIcon} alt='supportIcon' />
+                        </div>
                     </div>
                     <h5 className='pl-2 text-lg truncate text-white_2 font-medium' >{role}</h5>
                 </div>
