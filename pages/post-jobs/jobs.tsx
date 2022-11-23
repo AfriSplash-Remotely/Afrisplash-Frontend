c272585dc713267b9f653c81e710fa05132676dd
Patience <patience@ext.kobo360.com>
2022-11-23 11:26:09 +0100

    feat: add job-details form continuation

--- pages/post-jobs/jobs.tsx
+++ pages/post-jobs/jobs.tsx
@@ -1,9 +1,9 @@
+import React, { useState } from "react";
 import Confirmation from "components/molecules/JobConfirmation/JobConfirmation";
 import JobDetails from "components/molecules/JobDetails/JobDetails";
 import PackagePlan from "components/molecules/PackagePlan/PackagePlan";
 import Timeline from "components/molecules/Timeline/Timeline";
 import GeneralLayout from "layouts/generalLayout";
-import React, { useState } from "react";
 import styles from "../../styles/Post-jobs.module.scss";
 
 const PostJobsNotSigned = () => {
@@ -25,22 +25,22 @@ const PostJobsNotSigned = () => {
   };
 
   const jobDetails = () => {
-    setCurrentFormKey(1)
-  }
+    setCurrentFormKey(1);
+  };
   const packagePlan = () => {
-    setCurrentFormKey(2)
-  }
+    setCurrentFormKey(2);
+  };
   const confirmation = () => {
-    setCurrentFormKey(3)
-  }
+    setCurrentFormKey(3);
+  };
   const postJob = () => {
     if (currentFormKey === 1) {
-      return <JobDetails {...{renderPreviousForm, renderNextForm}} />;
+      return <JobDetails {...{ renderPreviousForm, renderNextForm }} />;
     } else if (currentFormKey === 2) {
-      return <PackagePlan {...{ renderNextForm}} />;
+      return <PackagePlan {...{ renderNextForm }} />;
     } else if (currentFormKey === 3) {
       return <Confirmation />;
-    } else if(currentFormKey == 0)
+    } else if (currentFormKey == 0)
       return (
         <div className="w-7/12 md:w-1/2 sm:w-3/4 mx-auto">
           <div className={`${styles.midSection} my-5 space-y-3`}>
@@ -60,8 +60,13 @@ const PostJobsNotSigned = () => {
 
           <form>
             <div className={styles.inputIcons}>
-              <input placeholder="Paste link here" className={`${styles.input} lg:pl-12 md:pl-10 sm:bg-none sm:pl-1`} />
-              <button className={`${styles.button} w-1/4 lg:w-1/4 sm:w-1/2`}>Get details</button>
+              <input
+                placeholder="Paste link here"
+                className={`${styles.input} lg:pl-12 md:pl-10 sm:bg-none sm:pl-1`}
+              />
+              <button className={`${styles.button} w-1/4 lg:w-1/4 sm:w-1/2`}>
+                Get details
+              </button>
             </div>
             <div className={styles.createPostWrapper}>
               <p className={`${styles.create} mt-5 mb-10`}>
@@ -95,10 +100,18 @@ const PostJobsNotSigned = () => {
   return (
     <>
       <GeneralLayout>
-        <div className= 'w-full lg:w-full md:w-full sm:3/4 mx-auto'>
+        <div className="w-full lg:w-full md:w-full sm:3/4 mx-auto">
           <div className={styles.headerWrapper}>
-            <h2 className={`${styles.header} mt-5 text-center lg:text-5xl md:text-3xl sm:text-sm xs:text-xs`}>Post a new job</h2>
-            <p className={`${styles.subHeader} my-5 text-center lg:text-xl md:text-base sm:text-sm xs:text-xs`}>Find your next great hire</p>
+            <h2
+              className={`${styles.header} mt-5 text-center lg:text-5xl md:text-3xl sm:text-sm xs:text-xs`}
+            >
+              Post a new job
+            </h2>
+            <p
+              className={`${styles.subHeader} my-5 text-center lg:text-xl md:text-base sm:text-sm xs:text-xs`}
+            >
+              Find your next great hire
+            </p>
           </div>
           <div>
             <Timeline
@@ -117,7 +130,7 @@ const PostJobsNotSigned = () => {
                 postJob,
                 jobDetails,
                 packagePlan,
-                confirmation
+                confirmation,
               }}
             />
           </div>
