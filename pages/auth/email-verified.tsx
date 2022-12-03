9c4c0df6b55672fed02fe1149b3620dbd3bd720c
Patience <patience@ext.kobo360.com>
2022-12-03 04:39:51 +0100

    fix: verify email and email-verified email pages and made sign up page responsive

--- pages/auth/email-verified.tsx
+++ pages/auth/email-verified.tsx
@@ -10,7 +10,7 @@ function EmailVerifiedPage() {
         <div className="w-fit mr-auto ml-auto">
           <Logo />
         </div>
-        <div className="max-w-[660px] h-[739px] l-[320px] bg-[#fff] md:shadow-[4px_4px_32px_rgba(0,0,0,0.1)] md:backdrop-opacity-20 rounded-[24px] w-full mt-12 flex box-border mr-auto ml-auto">
+        <div className="max-w-[560px] h-[739px] l-[320px] bg-[#fff] md:shadow-[4px_4px_32px_rgba(0,0,0,0.1)] md:backdrop-opacity-20 rounded-[24px] w-full mt-12 flex box-border mr-auto ml-auto">
           <div className="p-1">
             <VectorBox2 /> <EmailVerified />
           </div>
