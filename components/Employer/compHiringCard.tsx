e2982fa89bb426089e992d16221074772bf93bf4
sammy-code98 <sammysmart98@yahoo.com>
2023-01-01 21:50:04 +0100

    addcards

--- components/Employer/compHiringCard.tsx
+++ components/Employer/compHiringCard.tsx
@@ -1,20 +1,60 @@
 import React from 'react'
 import Image from 'next/image'
+import PropTypes, { InferProps } from "prop-types";
+import Button from 'components/atoms/Button/Button'
+import Institution from "assets/general/institution.svg"
+import Locattion from "assets/general/location.svg"
 import LogoWhite from "assets/general/logo-bg-white.svg"
-export default function CompHiringCard(): JSX.Element {
+import People from "assets/general/people.svg"
+
+
+const CompHiringCardProps = {
+    title: PropTypes.string.isRequired,
+    desc: PropTypes.string,
+    inst: PropTypes.string,
+    location: PropTypes.string,
+    people: PropTypes.string
+}
+
+export default function CompHiringCard({ title, desc, inst, location, people }: InferProps<typeof CompHiringCardProps>): JSX.Element {
     return (
-        <div className='border-2 border-primary_grey  rounded-lg p-4'>
+        <div className='border-2 border-primary_grey  rounded-lg p-14'>
             <div className='flex flex-col justify-center  items-center'>
                 <Image src={LogoWhite} alt='logo' height='30%' />
 
                 <div className='mt-4 py-2'>
-                    <h2 className='text-2xl text-center font-medium  '>Afrisplash Remotely</h2>
-                    <p className='text-center mt-3'>The gateway to Africa s remote workforce: Africa s #1 remote work community
-                        and largest open source remote work
-                        marketplace</p>
+                    <h2 className='text-2xl text-center font-medium'>{title}</h2>
+                    <p className='text-center mt-3'>{desc}</p>
                 </div>
 
-                <div></div>
+                <div className='mt-4'>
+                    <div className='flex items-center justify-center '>
+                        <div className='flex'>
+                            <Image src={Institution} alt='icon' height='5%' />
+                            <h4>{inst}</h4>
+                        </div>
+                        <div className='flex '>
+                            <Image src={Locattion} alt='icon' height='5%' />
+                            <h4>{location}</h4>
+                        </div>
+
+                    </div>
+                    <div className='flex justify-center items-center mt-6'>
+                        <Image src={People} alt='icon' height='2%' />
+                        <h4>{people}</h4>
+
+                    </div>
+                </div>
+                <div className='mt-6'>
+                    <Button
+                        type='filled'
+                        bgColor='primary_green'
+                        color='white'
+                        text='12 open roles'
+                        classes="w-64 h-10  md:w-64 xl:w-64 rounded-md text-sm capitalize  text-white bg-primary_green hover:opacity-80"
+
+                    />
+                </div>
 
             </div>
         </div>
