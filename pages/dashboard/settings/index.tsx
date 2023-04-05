4941cf2ef10b9d37918e68efa590acbda3757c48
EddyKelvin <emmajnr1000@gmail.com>
2023-04-05 16:46:49 +0100

    fix: styling

--- pages/dashboard/settings/index.tsx
+++ pages/dashboard/settings/index.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Image from 'next/image';
-import AccountPlaceholder from "../../../assets/account-placeholder.png";
+import AccountPlaceholder from "@/assets/account-placeholder.png";
 import styles from "../../../styles/Account.module.scss";
 import SettingsLayout from "@/layouts/settingsLayout";
 
@@ -10,15 +10,15 @@ import SettingsLayout from "@/layouts/settingsLayout";
 const Account = (): JSX.Element => {
   return (
     <SettingsLayout>
-      <div className={`w-[733px] bg-white rounded-[12px] pl-[62px] pt-[43px]`}>
+      <div className={`w-full bg-white rounded-xl p-4 md:p-12 `}>
         {/**Image Container */}
-        <div className={`flex items-center mb-[31px]`}>
+        <div className={`flex flex-wrap gap-1 items-center mb-8`}>
           <Image
             src={AccountPlaceholder}
             alt=""
           />
           <button
-            className={`text-white bg-[#0D5520] rounded-[9px] px-[36px] py-[12px] ml-[23px] mr-[12px]`}
+            className={`text-white bg-[#0D5520] rounded-lg px-[36px] py-[12px] ml-[23px] mr-[12px]`}
           >
             Upload New
           </button>
@@ -38,79 +38,79 @@ const Account = (): JSX.Element => {
           {/** Name Container */}
           <div className={`${styles.nameContainer}`}>
             {/**Last Name */}
-            <div className={`w-[46%] lg:w-[44%] mb-[22px]`}>
+            <div className={`w-full mb-[22px]`}>
               <div className={`${styles.separator}`}>
                 <label
-                  className={`text-[#292D32] text-[14px] font-[400] pb-[6px]`}
+                  className={`text-[#292D32] text-[14px]  font-normal  pb-[6px]`}
                 >
                   Last Name
                 </label>
                 <input
                   type="text"
-                  className={`border border-solid border-[#979797] w-[302px] rounded-[9px] h-[46px]`}
+                  className={`border border-solid border-[#979797] w-full rounded-lg h-[46px]`}
                 />
               </div>
             </div>
 
             {/**First Name */}
-            <div className={`w-[46%] lg:w-[47%] mb-[22px]`}>
-              <label className={`text-[#292D32] text-[14px] font-[400] pb-[6px]`}>
+            <div className={`w-full mb-[22px]`}>
+              <label className={`text-[#292D32] text-[14px]  font-normal  pb-[6px]`}>
                 First Name
               </label>
               <input
                 type="text"
-                className={`border border-solid border-[#979797] w-[302px] rounded-[9px] h-[46px]`}
+                className={`border border-solid border-[#979797] w-full rounded-lg h-[46px]`}
               />
             </div>
           </div>
 
           <div className={`${styles.nameContainer}`}>
             {/**Location */}
-            <div className={`w-[46%] lg:w-[44%] mb-[22px]`}>
+            <div className={`w-full mb-[22px]`}>
               <div className={`${styles.separator}`}>
                 <label
-                  className={`text-[#292D32] text-[14px] font-[400] pb-[6px]`}
+                  className={`text-[#292D32] text-[14px]  font-normal  pb-[6px]`}
                 >
                   Location
                 </label>
                 <input
                   type="text"
-                  className={`border border-solid border-[#979797] w-[302px] rounded-[9px] h-[46px]`}
+                  className={`border border-solid border-[#979797] w-full rounded-lg h-[46px]`}
                 />
               </div>
             </div>
 
             {/**Phone Number */}
-            <div className={`w-[46%] lg:w-[47%] mb-[22px]`}>
-              <label className={`text-[#292D32] text-[14px] font-[400] pb-[6px]`}>
+            <div className={`w-full mb-[22px]`}>
+              <label className={`text-[#292D32] text-[14px]  font-normal  pb-[6px]`}>
                 Phone Number
               </label>
               <input
                 type="number"
-                className={`border border-solid border-[#979797] w-[302px] rounded-[9px] h-[46px]`}
+                className={`border border-solid border-[#979797] w-full rounded-lg h-[46px]`}
               />
             </div>
           </div>
 
           {/**Email */}
           <div className={`flex flex-col`}>
-            <label className={`text-[#292D32] text-[14px] font-[400] pb-[6px]`}>
+            <label className={`text-[#292D32] text-sm font-normal pb-1`}>
               Email Address
             </label>
             <input
               type="email"
-              className={`border border-solid border-[#979797] w-[655px] rounded-[9px] h-[46px]`}
+              className={`border border-solid border-[#979797] w-full rounded-lg h-[46px]`}
             />
           </div>
 
-          <div className={`flex flex-row gap-[28px] pt-[45px]`}>
+          <div className={`flex flex-row gap-6 py-8`}>
             <button
-              className={`border-0 rounded-[9px] text-[15px] font-light text-[white] bg-[#0D5520] py-[11px] px-[59px]`}
+              className={`border-0 rounded-lg text-[15px] font-light text-[white] bg-[#0D5520] py-2  px-6`}
             >
               Save Changes
             </button>
             <button
-              className={`border border-solid border-[#0D5520] rounded-[9px] text-[#0D5520] py-[11px] px-[59px]`}
+              className={`border border-solid border-[#0D5520] rounded-lg text-[#0D5520] py-2  px-6`}
             >
               Discard
             </button>
