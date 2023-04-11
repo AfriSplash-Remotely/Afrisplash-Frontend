e30154e7e9b13f90e2a41d33397e00d6218eed81
EddyKelvin <emmajnr1000@gmail.com>
2023-04-11 01:13:00 +0100

    feat: add post

--- api/blog/blog.api.ts
+++ api/blog/blog.api.ts
@@ -1,9 +1,16 @@
 import axios from "@/utils/axios";
-import { blogI } from "./blog.interface";
+import { blogI, singleBlogI } from "./blog.interface";
 
 export const getAllBlogs = async (): Promise<blogI[]> => {
   const {
     data: { data },
   } = await axios.get("/blog/posts");
+  return data;
+};
+
+export const getBlog = async (blogUrl: string): Promise<singleBlogI> => {
+  const {
+    data: { data },
+  } = await axios.get(`/blog/post/${blogUrl}`);
   return data;
 };
