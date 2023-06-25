bac54a1ca64a937f09098eef2dc2a93defe71245
chimdie <nzeyoungwealth@gmail.com>
2023-06-25 12:34:28 +0100

    wip: move naviagtion sattes and handlers to the dashboard layout

--- layouts/adminLayout.tsx
+++ layouts/adminLayout.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import PropTypes, { InferProps } from "prop-types";
 import AdminNavigation from "components/molecules/Navigation/AdminNavigation";
+import MobileSidebar from "@/components/molecules/Navigation/MobileSidebar";
 import TopAdmin from "components/molecules/TopAdmin/TopAdmin";
 import avatar from "assets/admin_avatars/admin_avatar.svg";
 import styles from "./Layout.module.scss";
@@ -15,13 +17,34 @@ const AdminLayout: NextPage<InferProps<typeof AdminLayoutProps>> = ({
   children,
 }) => {
   const router = useRouter();
+  const [navSwitch, setNavSwitch] = useState(false);
+  const [focused, setFocused] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (window.innerWidth < 840) {
+      setNavSwitch(true);
+    }
+  }, [navSwitch, setNavSwitch]);
+
+  const handleNavSwitch: () => void = (): void => {
+    setNavSwitch(!navSwitch);
+  };
 
   return (
     <div className="w-full flex">
-      <AdminNavigation />
-
+      <AdminNavigation
+        focused={focused}
+        navSwitch={navSwitch}
+        setFocused={setFocused}
+        handleNavSwitch={handleNavSwitch}
+      />
       <div className={`${styles.main} w-full `}>
         <div className="w-11/12 mx-auto">
+          <MobileSidebar
+            focused={focused}
+            navSwitch={navSwitch}
+            setFocused={setFocused}
+          />
           <TopAdmin
             placeholder={`${
               router.pathname == "/dashboard/forum"
