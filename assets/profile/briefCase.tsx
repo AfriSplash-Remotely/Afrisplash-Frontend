477bb8b066adc2d2c63d7d12f297134d5954ee1d
chimdie <nzeyoungwealth@gmail.com>
2023-06-27 01:38:30 +0100

    feat: assets for user profile page

---
+++ assets/profile/briefCase.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from "react";
+
+export default function BriefCase() {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="16" height="16" rx="4" fill="#D6ECDC" />
+      <path
+        d="M7.99848 10.7548C8.98032 10.7548 9.77626 9.95884 9.77626 8.977C9.77626 7.99516 8.98032 7.19922 7.99848 7.19922C7.01664 7.19922 6.2207 7.99516 6.2207 8.977C6.2207 9.95884 7.01664 10.7548 7.99848 10.7548Z"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.47543 9.47049L7.5332 8.53271"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.46566 8.5415L7.52344 9.48373"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.22193 12.4436H9.77748C11.5642 12.4436 11.8842 11.7281 11.9775 10.857L12.3108 7.30141C12.4308 6.21696 12.1197 5.33252 10.2219 5.33252H5.77748C3.87971 5.33252 3.56859 6.21696 3.68859 7.30141L4.02193 10.857C4.11526 11.7281 4.43526 12.4436 6.22193 12.4436Z"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.2207 5.33247V4.97691C6.2207 4.19024 6.2207 3.55469 7.64293 3.55469H8.35404C9.77626 3.55469 9.77626 4.19024 9.77626 4.97691V5.33247"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.2879 7.55469C11.519 8.11469 10.6657 8.5058 9.78125 8.72802"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3.83008 7.6748C4.5723 8.18147 5.38119 8.54147 6.22119 8.74592"
+        stroke="#0D5520"
+        strokeWidth="0.8"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ assets/profile/clock.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from "react";
+
+export default function ClockSvg() {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="16" height="16" rx="4" fill="#D6ECDC" />
+      <path
+        d="M12.4436 7.99913C12.4436 10.4525 10.4525 12.4436 7.99913 12.4436C5.5458 12.4436 3.55469 10.4525 3.55469 7.99913C3.55469 5.5458 5.5458 3.55469 7.99913 3.55469C10.4525 3.55469 12.4436 5.5458 12.4436 7.99913Z"
+        stroke="#0D5520"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.64732 9.41279L8.26954 8.59057C8.02954 8.44835 7.83398 8.10613 7.83398 7.82613V6.00391"
+        stroke="#0D5520"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
