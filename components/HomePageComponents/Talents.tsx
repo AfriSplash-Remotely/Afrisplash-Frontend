4ab183af27a30740be8b856d640182c49e341385
AJ Creativity <ajcreativity471@gmail.com>
2023-07-02 19:59:30 +0100

    feat: implemented i18n Internalization and localization on landing page

--- components/HomePageComponents/Talents.tsx
+++ components/HomePageComponents/Talents.tsx
@@ -6,16 +6,25 @@ import Button from "@/components/atoms/Button/Button";
 import Link from "next/link";
 import { mentorsData, topTalentData } from "utils/fakeData";
 import styles from "./styles.module.scss";
+import { TFunction } from "i18next";
 
-function Talents(): JSX.Element {
+interface Props {
+  translate: TFunction<"home", undefined>;
+}
+
+function Talents({ translate }: Props): JSX.Element {
   return (
     <section className="w-full ">
       <div className="py-28 space-y-24 w-11/12 xl:w-10/12 mx-auto">
         <section>
           <div className="text-center space-y-5">
-            <h3 className="capitalize text-5xl font-semibold mb-4 md:mb-7">our top talents</h3>
+            <h3 className="capitalize text-5xl font-semibold mb-4 md:mb-7">
+              {translate("our top talents")}
+            </h3>
             <div className="text-gray-700 text-lg flex justify-center">
-              <p className="text-xl">Find highly-skilled talents from our community</p>
+              <p className="text-xl">
+                {translate("Find highly-skilled talents from our community")}
+              </p>
             </div>
           </div>
           <div className=" my-10 space-y-6">
@@ -35,13 +44,22 @@ function Talents(): JSX.Element {
               >
                 <div className="flex justify-between items-start relative   pt-5">
                   <Image src={talentUser} alt="stars" width={60} height={60} />
-                  <Image className="absolute right-0" src={talentStars} alt="stars" width={120} height={120} />
+                  <Image
+                    className="absolute right-0"
+                    src={talentStars}
+                    alt="stars"
+                    width={120}
+                    height={120}
+                  />
                 </div>
                 <div className="w-8/12 font-semibold leading-10 text-3xl">
-                  <h2>Become a top talent too</h2>
+                  <h2>{translate("Become a top talent too")}</h2>
                 </div>
                 <div>
-                  <Button classes={`bg-primary_green text-white py-4 px-6 rounded-lg`} text="Create your profile" />
+                  <Button
+                    classes={`bg-primary_green text-white py-4 px-6 rounded-lg`}
+                    text={translate("Create your profile")}
+                  />
                 </div>
               </div>
             </div>
@@ -60,7 +78,7 @@ function Talents(): JSX.Element {
           </div>
           <div className="w-max mx-auto">
             <Link href="/" className=" text-gray-400 leading-8 border-b-2">
-              View more
+              {translate("View more")}
             </Link>
           </div>
         </section>
