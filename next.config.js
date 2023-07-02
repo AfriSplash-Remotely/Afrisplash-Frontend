406c86b3d9aa593d2bdae3044926460d2545dd9e
AJ Creativity <ajcreativity471@gmail.com>
2023-07-02 19:59:30 +0100

    feat: implemented i18n Internalization and localization on landing page

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
+const { i18n } = require("./next-i18next.config");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
   // here goes your Next.js configuration
   reactStrictMode: true,
+  i18n,
   images: {
     domains: ["images.unsplash.com", "source.unsplash.com"],
-  }
+  },
 };
 
 const nextConfig = withPWA({
-  dest: 'public',
+  dest: "public",
   disable: !isProduction,
-})(
-  config
-);
+})(config);
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
