248d0012c8d938b45f03d02a33af56da14f715d5
copy-ninja1 <emmajnr1000@gmail.com>
2023-07-15 18:51:06 +0100

    fix: build error

--- next.config.js
+++ next.config.js
@@ -20,20 +20,5 @@ const nextConfig = withPWA({
   config
 );
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: "@mdx-js/react",
-  },
-});
-
-
 
 module.exports = nextConfig
-
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-});
