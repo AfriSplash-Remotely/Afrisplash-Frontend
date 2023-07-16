070b36be039faf887e6ef13821007f90b54f5ccc
copy-ninja1 <emmajnr1000@gmail.com>
2023-07-16 22:14:27 +0100

    fix: translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-i18next').UserConfig} */
 
 module.exports = {
+  // debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr", "ara", "sw", "zu", "ti", "ig", "ha", "yo"],
