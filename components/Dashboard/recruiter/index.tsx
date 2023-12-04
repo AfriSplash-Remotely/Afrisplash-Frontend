72058acda15420c0d8778a67e77978b4e802046a
sammy-code98 <sammysmart98@yahoo.com>
2023-12-04 18:45:03 +0100

    add recruiter dashboard

--- components/Dashboard/recruiter/index.tsx
+++ components/Dashboard/recruiter/index.tsx
@@ -1,10 +1,113 @@
+import DashboardCards from "@/components/atoms/DashboardCards/DashboardCards";
 import AdminLayout from "@/layouts/adminLayout";
+import { MdOutlineWorkOutline, MdOutlineBusinessCenter, MdMarkChatRead, MdGppBad } from "react-icons/md";
+import { Tab } from "@headlessui/react";
+import { jobData } from "@/utils";
+import JobCard from "@/components/jobCard";
+import Link from "next/link";
 
 const Recruiter = ():JSX.Element => {
+    const cardData = [
+        { title: 'Jobs', total: 30, icon: <MdOutlineBusinessCenter size='3rem' />, bgColor: '#D6ECDC' },
+        { title: 'Applicants', total: 30, icon: <MdOutlineWorkOutline size='3rem' />, bgColor: '#FDF1C9' },
+        { title: 'Interview Schedule', total: 30, icon: <MdMarkChatRead size='3rem' />, bgColor: '#F0FBF3' },
+        { title: 'Rejected', total: 30, icon: <MdGppBad size='3rem' />, bgColor: '#f87171' },
+    ]
+    const dash = ['Created Jobs', 'Applicants']
+
+    const classNames = (...classes: string[]) => {
+        return classes.filter(Boolean).join(" ");
+    };
+
     return (
         <AdminLayout>
             <div>
-                hello dashboard recruiter
+                <div>
+                    <h1 className="text-dark_black font-medium text-lg md:font-bold md:text-2xl lg:text-xl">Dashbaord</h1>
+                </div>
+                <div className="mt-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+                        {cardData.map((card) => {
+                            return <DashboardCards
+                                key={card.title}
+                                title={card.title}
+                                icon={card.icon}
+                                total={card.total}
+                                bgColor={card.bgColor} />
+                        })}
+                    </div>
+                </div>
+                <div className="mt-6">
+                    <Tab.Group>
+                        <Tab.List className='flex space-x-1 px-1 border-b-2 border-gray-100
+                        '>
+                            {dash.map((_dash) => {
+                                return (
+                                    <Tab key={_dash}
+                                        className={({ selected }) =>
+                                            classNames(
+                                                "w-full md:w-[20%] py-2.5 text-md font-medium leading-5 text-gray-800",
+                                                "focus:outline-none",
+                                                selected
+                                                    ? "bg-white border-b-4 border-primary_green text-primary_green"
+                                                    : "text-blue-800 hover:bg-primary_green/[0.1] hover:text-primary_green"
+                                            )
+                                        }
+                                    >
+                                        {_dash}
+                                    </Tab>
+                                )
+                            })}
+                        </Tab.List>
+                        <Tab.Panels className='mt-3'>
+                            <Tab.Panel>
+                                {jobData.map((job) => {
+                                    return (
+                                        <JobCard key={job.company}
+                                            image={job.image}
+                                            alt={"company image"}
+                                            company={job.company}
+                                            service={job.service}
+                                            employees={job.employees}
+                                            offer={job.offer}
+                                            salary={job.priceRange}
+                                            postDate={job.postDate}
+                                            status={job.hiring}
+                                            promoted={job.promoted}
+                                        />
+                                    )
+                                })}
+                                <div className="absolute right-12 py-2">
+                                    <Link href="/dashboard/jobs" className="text-primary_green text-base underline font-semibold">View All</Link>
+                                </div>
+
+                            </Tab.Panel>
+                            <Tab.Panel>
+                                {jobData.map((job) => {
+                                    return (
+                                        <JobCard key={job.company}
+                                            image={job.image}
+                                            alt={"company image"}
+                                            company={job.company}
+                                            service={job.service}
+                                            employees={job.employees}
+                                            offer={job.offer}
+                                            salary={job.priceRange}
+                                            postDate={job.postDate}
+                                            status={job.hiring}
+                                            promoted={job.promoted}
+                                        />
+                                    )
+                                })}
+                                <div className="absolute right-12 py-2">
+                                    <Link href="/dashboard/applied" className="text-primary_green text-base underline font-semibold">View All</Link>
+                                </div>
+                            </Tab.Panel>
+
+                        </Tab.Panels>
+                    </Tab.Group>
+
+                </div>
             </div>
         </AdminLayout>
     )
