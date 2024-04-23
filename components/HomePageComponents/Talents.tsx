7b6541cadd98ebb2ed737b751cdfbe6ef343ddce
Victor AJ <ajcreativity471@gmail.com>
2024-04-23 10:07:16 +0100

    Removed redundant datas and implemented requested changes

--- components/HomePageComponents/Talents.tsx
+++ components/HomePageComponents/Talents.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 import { TFunction } from "i18next";
 import Link from "next/link";
 import talentStars from "assets/home-page/talent_stars_bg.svg";
 import talentUser from "assets/home-page/talent_user.svg";
 import { MentorCard } from "@/components/Forum/MentorCardComponent";
 import Button from "@/components/atoms/Button/Button";
-import { mentorsData } from "utils/fakeData";
 import styles from "./styles.module.scss";
 import { getTotalCandidates } from "@/api-endpoints/get-candidates/get-candidates.api";
 import GhanaFlag from "assets/general/ghana-flag.svg";
@@ -16,6 +17,8 @@ interface Props {
 }
 
 function Talents({ translate }: Props): JSX.Element {
+  const route = useRouter();
+
   const { data } = useQuery(["candidatesList"], async () => {
     const response = await getTotalCandidates();
     return response.data;
@@ -37,54 +40,84 @@ function Talents({ translate }: Props): JSX.Element {
           </div>
           <div className=" my-10 space-y-6">
             <div className="grid gap-6  s:grid-cols-1 lg:grid-cols-4">
-              {data && data.map((candidate) => {
-                const fullName = `${candidate.first_name} ${candidate.last_name}`;
-                return (
-                  <MentorCard
-                    key={candidate._id}
-                    name={fullName}
-                    position='Senior Developer'
-                    role={candidate.role}
-                    flag={GhanaFlag}
-                    src={candidate.profile_image}
-                  />
-                );
-              })}
-              <div
-                className={`w-full rounded-xl p-4 bg-cover shadow-sm  bg-no-repeat space-y-10 mt-2 ${styles.createProfileCard}`}
-              >
-                <div className="flex justify-between items-start relative   pt-5">
-                  <Image src={talentUser} alt="stars" width={60} height={60} />
-                  <Image
-                    className="absolute right-0"
-                    src={talentStars}
-                    alt="stars"
-                    width={120}
-                    height={120}
-                  />
+              {data && data.length > 0 ? (
+                data &&
+                data.map((candidate, index) => {
+                  const fullName = `${candidate.first_name} ${candidate.last_name}`;
+                  return (
+                    <React.Fragment key={candidate._id}>
+                      <MentorCard
+                        name={fullName}
+                        position="Senior Developer"
+                        role={candidate.role}
+                        flag={GhanaFlag}
+                        src={candidate.profile_image}
+                      />
+                      {index == 2 && (
+                        <div
+                          className={`w-full rounded-xl p-4 bg-cover shadow-sm  bg-no-repeat space-y-10 mt-2 ${styles.createProfileCard}`}
+                        >
+                          <div className="flex justify-between items-start relative pt-5">
+                            <Image
+                              src={talentUser}
+                              alt="stars"
+                              width={60}
+                              height={60}
+                            />
+                            <Image
+                              className="absolute right-0"
+                              src={talentStars}
+                              alt="stars"
+                              width={120}
+                              height={120}
+                            />
+                          </div>
+                          <div className="w-8/12 font-semibold leading-10 text-3xl">
+                            <h2>{translate("Become a top talent too")}</h2>
+                          </div>
+                          <div className="pt-6">
+                            <Button
+                              onClick={() => route.push("/auth/signup")}
+                              classes={`bg-primary_green text-white py-4 px-6 rounded-lg`}
+                              text={translate("Create your profile")}
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </React.Fragment>
+                  );
+                })
+              ) : (
+                <div
+                  className={`w-full rounded-xl p-4 bg-cover shadow-sm  bg-no-repeat space-y-10 mt-2 ${styles.createProfileCard}`}
+                >
+                  <div className="flex justify-between items-start relative pt-5">
+                    <Image
+                      src={talentUser}
+                      alt="stars"
+                      width={60}
+                      height={60}
+                    />
+                    <Image
+                      className="absolute right-0"
+                      src={talentStars}
+                      alt="stars"
+                      width={120}
+                      height={120}
+                    />
+                  </div>
+                  <div className="w-8/12 font-semibold leading-10 text-3xl">
+                    <h2>{translate("Become a top talent too")}</h2>
+                  </div>
+                  <div>
+                    <Button
+                      onClick={() => route.push("/auth/signup")}
+                      classes={`bg-primary_green text-white py-4 px-6 rounded-lg mt-6`}
+                      text={translate("Create your profile")}
+                    />
+                  </div>
                 </div>
-                <div className="w-8/12 font-semibold leading-10 text-3xl">
-                  <h2>{translate("Become a top talent too")}</h2>
-                </div>
-                <div>
-                  <Button
-                    classes={`bg-primary_green text-white py-4 px-6 rounded-lg`}
-                    text={translate("Create your profile")}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="grid gap-6 grid-cols-1 sm:grid-cols-1  lg:grid-cols-4">
-              {mentorsData.map((mentor) => (
-                <MentorCard
-                  key={mentor.id}
-                  name={mentor.name}
-                  position={mentor.position}
-                  role={mentor.role}
-                  flag={mentor.flag}
-                  src={mentor.src}
-                />
-              ))}
+              )}
             </div>
           </div>
           <div className="w-max mx-auto">
