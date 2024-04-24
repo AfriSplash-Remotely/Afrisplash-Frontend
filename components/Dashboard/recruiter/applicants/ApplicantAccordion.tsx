bc63d4d931891802b164f7f7b70f91f34565d0e8
Chimdie <nzeyoungwealth@gmail.com>
2024-04-24 09:38:02 +0100

    wip: applicants endpooint integration

--- components/Dashboard/recruiter/applicants/ApplicantAccordion.tsx
+++ components/Dashboard/recruiter/applicants/ApplicantAccordion.tsx
@@ -3,18 +3,10 @@ import Image from "next/image";
 import { FcApproval } from "react-icons/fc";
 import { HiOutlineChevronUp, HiOutlineChevronDown } from "react-icons/hi2";
 import ResumeModal from "./ResumeModal";
+import { RecruiterJobData } from "@/api-endpoints/jobs/jobs.interface";
+import pic9 from "assets/images/pic9.png";
 
-type ApplicantAccordionT = {
-	jobTitle: string;
-	desc: string;
-	candidate: {
-		id: string;
-		name: string;
-		avatar: string;
-	}[];
-};
-
-export const ApplicantAccordion = (props: ApplicantAccordionT): JSX.Element => {
+export const ApplicantAccordion = (props: Partial<RecruiterJobData>): JSX.Element => {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const [isModalOpen, setIsodalOpen] = useState<boolean>(false);
 
@@ -27,19 +19,23 @@ export const ApplicantAccordion = (props: ApplicantAccordionT): JSX.Element => {
 			<div className="border border-grey_2 rounded space-y-6 p-4 divide-y">
 				<button
 					className="w-full transition-all ease-in duration-200"
-					onClick={() => setIsOpen(!isOpen)}
+					onClick={() => {
+						setIsOpen(!isOpen)
+						console.log({ id: props._id });
+
+					}}
 				>
 					<div className="flex justify-between">
-						<div className="flex space-x-2 md:space-x-3">
+						<div className="flex space-x-2 md:space-x-3 w-full">
 							<div className="h-8 w-8 md:h-16 md:w-16 aspect-square rounded-xl flex items-center justify-center -border border-grey_2 shadow">
 								<FcApproval className="md:text-4xl" />
 							</div>
-							<div className="flex flex-col items-start">
-								<span className="text-base md:text-xl font-bold">
-									{props.jobTitle}
+							<div className="flex flex-col items-start w-5/6 -border border-red-400">
+								<span className="text-base md:text-lg font-semibold capitalize">
+									{props.title}
 								</span>
 								<span className="text-xs md:text-base font-normal text-left">
-									{props.desc}
+									{props.description}
 								</span>
 							</div>
 						</div>
@@ -52,11 +48,11 @@ export const ApplicantAccordion = (props: ApplicantAccordionT): JSX.Element => {
 							List of candidates who applied for this job
 						</span>
 						<div className="space-y-4">
-							{props.candidate.map((c) => {
+							{props.applicants?.map((c) => {
 								return (
-									<div key={c.id} className="flex items-center space-x-3">
+									<div key={c._id} className="flex items-center space-x-3">
 										<Image
-											src={c.avatar}
+											src={pic9.src}
 											width={200}
 											height={200}
 											alt=""
@@ -64,10 +60,14 @@ export const ApplicantAccordion = (props: ApplicantAccordionT): JSX.Element => {
 										/>
 										<div className="flex flex-col items-start justify-between">
 											<span className="text-xs md:text-sm font-semibold">
-												{c.name}
+												{c._user}
 											</span>
 											<button
-												onClick={onClose}
+												onClick={() => {
+													onClose()
+													console.log({ resumeid: c._id });
+
+												}}
 												className="text-[10px] md:text-xs font-thin underline"
 											>
 												View resume
