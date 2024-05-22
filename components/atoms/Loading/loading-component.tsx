6fd48209eff222d6ce60af5f08ee8d4d544921c2
sammy-code98 <sammysmart98@yahoo.com>
2024-05-22 11:15:12 +0100

    fix:resolve lint issue causing vercel build break

--- components/atoms/Loading/loading-component.tsx
+++ components/atoms/Loading/loading-component.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-const LoadingComponent = () => {
+const LoadingComponent = (): JSX.Element => {
     return (
         <div className="flex flex-col space-y-4 w-full">
             <div className="bg-slate-200 h-8 w-full animate-pulse"></div>
