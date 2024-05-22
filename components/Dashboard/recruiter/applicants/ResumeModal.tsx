e9c7d12e4c21cb21b3cba0c64df470fc85c1aafb
Chimdie <nzeyoungwealth@gmail.com>
2024-05-22 08:55:53 +0100

    fix: resume modal

--- components/Dashboard/recruiter/applicants/ResumeModal.tsx
+++ components/Dashboard/recruiter/applicants/ResumeModal.tsx
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import {
-	IoCloseCircleOutline,
-	IoArrowDownCircleOutline,
-} from "react-icons/io5";
-import { BsTelephone, BsBriefcase } from "react-icons/bs";
-import { MdOutlineAttachEmail, MdOutlineLocationOn } from "react-icons/md";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import Modal, { ModalT } from "@/components/molecules/Modal";
-import pic9 from "assets/images/pic9.png";
-import Image from "next/image";
-import { IconType } from "react-icons";
 import RejectionModal from "./RejectionModal";
 
 type ModalProps = {
@@ -22,17 +16,25 @@ export default function ResumeModal({
 	setIsodalOpen,
 }: ModalProps): JSX.Element {
 	const [isRejectionOpen, setOpenRejection] = useState<boolean>(false);
+	const [copiedEmail, setCopiedEmail] = useState<string>("");
+	const [isEmailCopied, setIsEmailCopied] = useState<boolean>(false);
 
 	const handleOpenReject = () => {
 		setIsodalOpen(!open);
 		setOpenRejection(!isRejectionOpen);
 	};
 
+	const onCopyText = () => {
+		setIsEmailCopied(true);
+		setTimeout(() => setIsEmailCopied(false), 2000);
+	};
+
+
 	return (
 		<>
 			<Modal onClose={onClose} open={open}>
 				<div className="bg-white">
-					<div className="flex items-center overflow-hidden space-x-6 bg-grey_2/40 pt-6 pb-3 px-6">
+					<div className="flex items-center overflow-hidden space-x-6 bg-white-smoke pt-6 pb-3 px-6">
 						<button onClick={onClose}>
 							<AiOutlineClose className="text-lg font-medium" />
 						</button>
@@ -43,100 +45,62 @@ export default function ResumeModal({
 
 					<div className="px-8 py-6 space-y-6">
 						<div className="space-y-4">
-							<div className="bg-grey_2 p-6 flex items-center space-x-4 -border border-red-500">
-								<Image
-									src={pic9.src}
-									width={200}
-									height={200}
-									alt=""
-									className="w-10 h-10 md:w-20 md:h-20 rounded-full aspect-square"
-								/>
-								<div className="flex flex-col ">
-									<span className="text-sm md:text-lg font-semibold">
-										Adegbite Damilola
-									</span>
-									<span className="text-xs md:text-sm font-normal">
-										UI UX Designer
-									</span>
-								</div>
-							</div>
-							<div className="md:px-6">
-								<p className="text-xs font-thin">
-									I am a self motivated person,Lorepum dolor sitget, consectetur
-									aiscingelit. Ne tsitget, consectetur adiinglor sitget,
-									consectetur aiscingddsfflor sitget, cons ectetur aiscinglor
-									sitget, consectetur aiscinglor sitgetfflor sitget,
-									consecteturh
-								</p>
-							</div>
-						</div>
-
-						<div className="space-y-4">
-							<div className="bg-grey_2 p-4">
-								<span className="text-base font-medium">Contact</span>
-							</div>
+							<span className="text-sm md:text-lg text-black font-medium">
+								Personal Information
+							</span>
 							<div className="space-y-2">
-								<ItemCard icon={BsTelephone} data="+234 8100661300" />
-								<ItemCard icon={BsBriefcase} data="www.behance.net.com" />
-								<ItemCard icon={MdOutlineAttachEmail} data="deebiz@gmail.com" />
-								<ItemCard icon={MdOutlineLocationOn} data="Lagos, Nigeria" />
+								<ItemCard title="First name" data="Precious" />
+								<ItemCard title="Last name" data="Francis" />
+								<ItemCard title="Email" data="deebiz@gmail.com" />
 							</div>
 						</div>
-
 						<div className="space-y-4">
-							<div className="bg-grey_2 p-4">
-								<span className="text-base font-medium">Education</span>
-							</div>
-							<div className="space-y-2 flex flex-col">
-								<span className="text-sm font-normal">
-									Bachelor of Arts (Accounting)
-								</span>
-								<span className="text-sm font-normal text-grey">
-									University of Benin
-								</span>
-								<span className="text-sm font-normal text-grey">2017-2021</span>
+							<span className="text-sm md:text-lg text-black font-medium">
+								Other Information
+							</span>
+							<div className="space-y-2">
+								<ItemCard title="Job Title" data="Product Designer" />
+								<ItemCard title="Country" data="Nigeria" />
+								<ItemCard title="Phone number" data="+234 112 567 8903" />
+								<ItemCard title="Expected salary" data="$40,000 - $200,000" />
 							</div>
 						</div>
-
 						<div className="space-y-4">
-							<div className="bg-grey_2 p-4">
-								<span className="text-base font-medium">Work Experience</span>
-							</div>
-							<div className="space-y-2 flex flex-col">
-								<span className="text-sm font-normal">
-									Product Designer at Afrisplash
-								</span>
-								<li>
-									<span className="text-sm font-normal text-grey">
-										April 2021 - September 2021
-									</span>
-								</li>
+							<span className="text-sm md:text-lg text-black font-medium">
+								Resume
+							</span>
+							<div className="flex items-center space-x-6">
+								<div className="flex items-center space-x-2">
+									<AttachSquare /> <span className="text-sm md:text-lg font-normal text-black">Someone’s CV.pdf</span>
+								</div>
+								<button className="text-primary_green text-sm md:text-lg font-normal underline">
+									Download
+								</button>
 							</div>
 						</div>
 					</div>
 					<div className="border-t border-grey_2 p-6">
-						<div className="flex space-x-2">
-							<button
-								type="button"
-								className="inline-flex items-center justify-center space-x-2 py-2 px-4 border border-grey_4 rounded-lg text-sm md:text-base font-normal"
-							>
-								<IoArrowDownCircleOutline />
-								<span>Download resume</span>
-							</button>
+						<div className="flex space-x-2 justify-center">
 							<button
 								type="button"
 								className="inline-flex items-center justify-center space-x-2 py-2 px-4 bg-red-600 text-white border border-red-600 rounded-lg text-sm md:text-base font-normal"
+
 								onClick={handleOpenReject}
 							>
 								<IoCloseCircleOutline />
-								<span>Reject</span>
+								<span className="text-sm">Reject</span>
 							</button>
-							<button
-								type="button"
-								className="inline-flex items-center justify-center space-x-2 py-2 px-4 bg-green-800 text-white border border-green-800 rounded-lg text-sm md:text-base font-normal"
-							>
-								View Profile
-							</button>
+							<CopyToClipboard text={copiedEmail} onCopy={onCopyText}>
+								<button
+									type="button"
+									onClick={() => setCopiedEmail("deebiz@gmail.com")}
+									className={`inline-flex items-center justify-center space-x-2 py-2 px-4 ${isEmailCopied === true ? "bg-green-800/80" : "bg-green-800"
+										} text-white border border-green-800 rounded-lg text-sm md:text-base font-normal`}
+								>
+									<MdOutlineMarkEmailRead />
+									<span className="text-sm">Copy email</span>
+								</button>
+							</CopyToClipboard>
 						</div>
 					</div>
 				</div>
@@ -145,14 +109,29 @@ export default function ResumeModal({
 		</>
 	);
 }
-// handleOpenReject={handleOpenReject}
 
-const ItemCard = ({ icon, data }: { icon: IconType; data: string }) => {
-	const Icon = icon;
+const ItemCard = ({ title, data }: { title: string; data: string }) => {
 	return (
-		<div className="flex items-center space-x-2">
-			{<Icon />}
-			<span className="text-sm font-normal">{data}</span>
+		<div className="flex items-center space-x-6">
+			<span className="text-xs md:text-sm text-black font-medium">{title}</span>
+			<span className="text-xs md:text-sm text-black font-normal">{data}</span>
 		</div>
+	);
+};
+
+const AttachSquare = () => {
+	return (
+		<svg
+			width="25"
+			height="24"
+			viewBox="0 0 25 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M16.69 2H8.31C4.67 2 2.5 4.17 2.5 7.81V16.18C2.5 19.83 4.67 22 8.31 22H16.68C20.32 22 22.49 19.83 22.49 16.19V7.81C22.5 4.17 20.33 2 16.69 2ZM16.87 14.35L14.65 16.57C14.11 17.11 13.41 17.37 12.71 17.37C12.01 17.37 11.3 17.1 10.77 16.57C10.25 16.05 9.96 15.36 9.96 14.63C9.96 13.9 10.25 13.2 10.77 12.69L12.18 11.28C12.47 10.99 12.95 10.99 13.24 11.28C13.53 11.57 13.53 12.05 13.24 12.34L11.83 13.75C11.59 13.99 11.46 14.3 11.46 14.63C11.46 14.96 11.59 15.28 11.83 15.51C12.32 16 13.11 16 13.6 15.51L15.82 13.29C17.09 12.02 17.09 9.96 15.82 8.69C14.55 7.42 12.49 7.42 11.22 8.69L8.8 11.11C8.29 11.62 8.01 12.29 8.01 13C8.01 13.71 8.29 14.39 8.8 14.89C9.09 15.18 9.09 15.66 8.8 15.95C8.51 16.24 8.03 16.24 7.74 15.95C6.94 15.18 6.5 14.13 6.5 13.01C6.5 11.89 6.93 10.84 7.72 10.05L10.14 7.63C11.99 5.78 15.01 5.78 16.86 7.63C18.72 9.48 18.72 12.5 16.87 14.35Z"
+				fill="#292D32"
+			/>
+		</svg>
 	);
 };
