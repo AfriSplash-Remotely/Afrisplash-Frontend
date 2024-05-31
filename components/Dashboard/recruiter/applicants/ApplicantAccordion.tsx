a1309b4025f3fd1e08ffebe012537987d7b03b90
Chimdie <nzeyoungwealth@gmail.com>
2024-05-31 01:34:45 +0100

    feat: adds job applicants listing

--- components/Dashboard/recruiter/applicants/ApplicantAccordion.tsx
+++ components/Dashboard/recruiter/applicants/ApplicantAccordion.tsx
@@ -3,12 +3,35 @@ import Image from "next/image";
 import { FcApproval } from "react-icons/fc";
 import { HiOutlineChevronUp, HiOutlineChevronDown } from "react-icons/hi2";
 import ResumeModal from "./ResumeModal";
-import { RecruiterJobData } from "@/api-endpoints/jobs/jobs.interface";
+import { JobApplicants, RecruiterJobData } from "@/api-endpoints/jobs/jobs.interface";
 import pic9 from "assets/images/pic9.png";
+import { useQuery } from "@tanstack/react-query";
+import { getApplicants } from "@/api-endpoints/jobs/jobs.api";
+import { useSession } from "next-auth/react";
 
-export const ApplicantAccordion = (props: Partial<RecruiterJobData>): JSX.Element => {
+export const ApplicantAccordion = (
+	props: Partial<RecruiterJobData>
+): JSX.Element => {
+	const { data: session } = useSession();
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const [isModalOpen, setIsodalOpen] = useState<boolean>(false);
+	const [jobId, setJobId] = useState<string | null>(null);
+	const [applicants, setApplicants] = useState<Pick<JobApplicants, 'data'> | null>(null)
+
+	const jwt = session?.user?.accessToken as unknown as string;
+
+	useQuery({
+		queryKey: ["applicants", jobId],
+		queryFn: () => {
+			return getApplicants(jobId as string, jwt)
+		},
+		onSuccess: (data: Pick<JobApplicants, 'data'>) => {
+			if (data) {
+				setApplicants(data);
+			}
+		},
+		enabled: !!jobId
+	});
 
 	const onClose = () => {
 		setIsodalOpen(!isModalOpen);
@@ -20,9 +43,8 @@ export const ApplicantAccordion = (props: Partial<RecruiterJobData>): JSX.Elemen
 				<button
 					className="w-full transition-all ease-in duration-200"
 					onClick={() => {
-						setIsOpen(!isOpen)
-						console.log({ id: props._id });
-
+						setIsOpen(!isOpen);
+						setJobId(props?._id as string);
 					}}
 				>
 					<div className="flex justify-between">
@@ -47,35 +69,53 @@ export const ApplicantAccordion = (props: Partial<RecruiterJobData>): JSX.Elemen
 						<span className="text-xs md:text-sm font-semibold">
 							List of candidates who applied for this job
 						</span>
-						<div className="space-y-4">
-							{props.applicants?.map((c) => {
-								return (
-									<div key={c._id} className="flex items-center space-x-3">
-										<Image
-											src={pic9.src}
-											width={200}
-											height={200}
-											alt=""
-											className="w-9 h-9 rounded-full aspect-square"
-										/>
-										<div className="flex flex-col items-start justify-between">
-											<span className="text-xs md:text-sm font-semibold">
-												{c._user}
-											</span>
-											<button
-												onClick={() => {
-													onClose()
-													console.log({ resumeid: c._id });
-
-												}}
-												className="text-xs md:text-xs font-thin underline text-black"
-											>
-												View Application
-											</button>
+						<div>
+							{!applicants?.data ? (
+								<h5 className="text-sm font-medium text-gray-300">
+									Loading...
+								</h5>
+							) : (
+								<>
+									{applicants?.data?.external_applicants.length === 0 ? (
+										<span className="text-xs font-medium text-gray-400">
+											No Applicants yet
+										</span>
+									) : (
+										<div className="transition-all duration-700  ease-in space-y-4">
+											{applicants?.data?.external_applicants?.map((c, index) => {
+												return (
+													<div
+														key={index}
+														className="flex items-center space-x-3"
+													>
+														<Image
+															src={pic9.src}
+															width={200}
+															height={200}
+															alt=""
+															className="w-9 h-9 rounded-full aspect-square"
+														/>
+														<div className="flex flex-col items-start justify-between">
+															<span className="text-xs md:text-sm font-semibold">
+																{`${c.firstName} ${c.lastName}`}
+															</span>
+															<button
+																onClick={() => {
+																	onClose();
+																	// console.log({ resumeid: c._id });
+																}}
+																className="text-xs md:text-xs font-thin underline text-black"
+															>
+																View Application
+															</button>
+														</div>
+													</div>
+												);
+											})}
 										</div>
-									</div>
-								);
-							})}
+									)}
+								</>
+							)}
 						</div>
 					</div>
 				) : null}
--- components/Dashboard/recruiter/applicants/ResumeModal.tsx
+++ components/Dashboard/recruiter/applicants/ResumeModal.tsx
@@ -5,6 +5,7 @@ import { MdOutlineMarkEmailRead } from "react-icons/md";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Modal, { ModalT } from "@/components/molecules/Modal";
 import RejectionModal from "./RejectionModal";
+import { AttachSquare } from "@/assets/svg/AttachSquareIcon";
 
 type ModalProps = {
 	setIsodalOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -28,7 +29,6 @@ export default function ResumeModal({
 		setIsEmailCopied(true);
 		setTimeout(() => setIsEmailCopied(false), 2000);
 	};
-
 
 	return (
 		<>
@@ -71,7 +71,10 @@ export default function ResumeModal({
 							</span>
 							<div className="flex items-center space-x-6">
 								<div className="flex items-center space-x-2">
-									<AttachSquare /> <span className="text-sm md:text-lg font-normal text-black">Someone’s CV.pdf</span>
+									<AttachSquare />{" "}
+									<span className="text-sm md:text-lg font-normal text-black">
+										Someone’s CV.pdf
+									</span>
 								</div>
 								<button className="text-primary_green text-sm md:text-lg font-normal underline">
 									Download
@@ -84,7 +87,6 @@ export default function ResumeModal({
 							<button
 								type="button"
 								className="inline-flex items-center justify-center space-x-2 py-2 px-4 bg-red-600 text-white border border-red-600 rounded-lg text-sm md:text-base font-normal"
-
 								onClick={handleOpenReject}
 							>
 								<IoCloseCircleOutline />
@@ -94,8 +96,9 @@ export default function ResumeModal({
 								<button
 									type="button"
 									onClick={() => setCopiedEmail("deebiz@gmail.com")}
-									className={`inline-flex items-center justify-center space-x-2 py-2 px-4 ${isEmailCopied === true ? "bg-green-800/80" : "bg-green-800"
-										} text-white border border-green-800 rounded-lg text-sm md:text-base font-normal`}
+									className={`inline-flex items-center justify-center space-x-2 py-2 px-4 ${
+										isEmailCopied === true ? "bg-green-800/80" : "bg-green-800"
+									} text-white border border-green-800 rounded-lg text-sm md:text-base font-normal`}
 								>
 									<MdOutlineMarkEmailRead />
 									<span className="text-sm">Copy email</span>
@@ -116,22 +119,5 @@ const ItemCard = ({ title, data }: { title: string; data: string }) => {
 			<span className="text-xs md:text-sm text-black font-medium">{title}</span>
 			<span className="text-xs md:text-sm text-black font-normal">{data}</span>
 		</div>
-	);
-};
-
-const AttachSquare = () => {
-	return (
-		<svg
-			width="25"
-			height="24"
-			viewBox="0 0 25 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-		>
-			<path
-				d="M16.69 2H8.31C4.67 2 2.5 4.17 2.5 7.81V16.18C2.5 19.83 4.67 22 8.31 22H16.68C20.32 22 22.49 19.83 22.49 16.19V7.81C22.5 4.17 20.33 2 16.69 2ZM16.87 14.35L14.65 16.57C14.11 17.11 13.41 17.37 12.71 17.37C12.01 17.37 11.3 17.1 10.77 16.57C10.25 16.05 9.96 15.36 9.96 14.63C9.96 13.9 10.25 13.2 10.77 12.69L12.18 11.28C12.47 10.99 12.95 10.99 13.24 11.28C13.53 11.57 13.53 12.05 13.24 12.34L11.83 13.75C11.59 13.99 11.46 14.3 11.46 14.63C11.46 14.96 11.59 15.28 11.83 15.51C12.32 16 13.11 16 13.6 15.51L15.82 13.29C17.09 12.02 17.09 9.96 15.82 8.69C14.55 7.42 12.49 7.42 11.22 8.69L8.8 11.11C8.29 11.62 8.01 12.29 8.01 13C8.01 13.71 8.29 14.39 8.8 14.89C9.09 15.18 9.09 15.66 8.8 15.95C8.51 16.24 8.03 16.24 7.74 15.95C6.94 15.18 6.5 14.13 6.5 13.01C6.5 11.89 6.93 10.84 7.72 10.05L10.14 7.63C11.99 5.78 15.01 5.78 16.86 7.63C18.72 9.48 18.72 12.5 16.87 14.35Z"
-				fill="#292D32"
-			/>
-		</svg>
 	);
 };
