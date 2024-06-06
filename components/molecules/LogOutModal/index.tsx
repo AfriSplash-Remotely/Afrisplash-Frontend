70b58d161c6e1c5083b2fa19da9064382d507ba8
Chimdie <nzeyoungwealth@gmail.com>
2024-06-06 20:24:11 +0100

    fix: text alignment for mobile screen

--- components/molecules/LogOutModal/index.tsx
+++ components/molecules/LogOutModal/index.tsx
@@ -14,8 +14,8 @@ export default function LogOutModal({ open, onClose, onLogOut, loading }: LogOut
     <Modal isOpen={open} setIsOpen={onClose} dialogPanelClass="w-2xl max-w-xl">
       <div className='flex flex-col justify-center items-center py-8'>
         <div className='space-y-4'>
-          <h2 className='text-2xl font-bold text-primary_green text-center'>Are you sure you want to log out?</h2>
-          <p className='text-lg text-center'>You're about to be logged out. Remember,your profile and application history are always available when you log back in.</p>
+          <h2 className='text-lg md:text-2xl font-bold text-primary_green text-center'>Are you sure you want to log out?</h2>
+          <p className='text-sm md:text-lg text-center'>You're about to be logged out. Remember,your profile and application history are always available when you log back in.</p>
         </div>
 
         <div className='mt-8 flex justify-between items-center gap-8 '>
@@ -34,7 +34,7 @@ export default function LogOutModal({ open, onClose, onLogOut, loading }: LogOut
               "bg-primary_green text-sm text-white px-6 md:px-12 py-2 rounded-lg w-1/2  md:w-auto"
             }
           >
-            <span className="flex gap-4 mx-auto item-center justify-center font-bold">{loading ? 'Loging Out ...' : 'Yes,Logout'}</span>
+            <span className="flex gap-4 mx-auto item-center justify-center font-bold">{loading ? 'Loging Out ...' : 'Logout'}</span>
           </Button>
         </div>
       </div>
